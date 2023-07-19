7e8b3da14e47e6bc6eeec4c15419a7a603c2c6a6
Tristan Keester <tristankeester@gmail.com>
2023-07-18 19:42:12 -0500

    Created CommentPostOverlay.jsx and moved all the overlay rendering into it

--- client/pages/Comments.jsx
+++ client/pages/Comments.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
 import { useParams } from 'react-router-dom';
 import HelperFunctions from '../helper-functions';
 import '../styles/Comments.scss';
 
 import { Navbar } from '../components/Navbar.jsx';
 import { CommentBox } from '../components/CommentBox.jsx';
+import { CommentPostOverlay } from '../components/CommentPostOverlay.jsx';
 
 
 const Comments = () => {
@@ -134,7 +134,25 @@ const Comments = () => {
 
   const openOverlay = (e) => {
     // e.preventDefault();
-    setShowOverlay(true);
+    if (showOverlay) {
+      setShowOverlay(null);
+    }
+    else {
+      const postCommentOverlay = <CommentPostOverlay
+        titleEntry={titleEntry}
+        setTitleEntry={setTitleEntry}
+        languageEntry={languageEntry}
+        setLanguageEntry={setLanguageEntry}
+        initialVal={initialVal}
+        handleEditorChange={handleEditorChange}
+        entry={entry}
+        setEntry={setEntry}
+        image={image}
+        setImage={setImage}
+        addComment={addComment}
+      />
+      setShowOverlay(postCommentOverlay);
+    }
   };
 
   const handleAccordionClick = (index) => {
@@ -147,12 +165,12 @@ const Comments = () => {
 
   const comments = mockData.map((item, index) => {
     return <CommentBox 
-              item={item}
-              index={index} 
-              activeIndex={activeIndex}
-              handleAccordionClick={handleAccordionClick}
-              HelperFunctions={HelperFunctions}
-            />
+      item={item}
+      index={index} 
+      activeIndex={activeIndex}
+      handleAccordionClick={handleAccordionClick}
+      HelperFunctions={HelperFunctions}
+    />
   });
 
   return (
@@ -170,91 +188,9 @@ const Comments = () => {
               </div>
             </div>
             <button className="button" onClick={openOverlay}>+ ADD POST</button>
-            {showOverlay && (
-              <div className="overlay-comments">
-                <div className="overlay-content-comments">
-                  <div>
-                    <form>
-                      <div className="formGroup-two">
-                        <div>
-                          <h2>Add FORM</h2>
-                          <hr className="line" />
-                          <input
-                            type="text"
-                            className="input-one-first"
-                            placeholder="Title"
-                            value={titleEntry}
-                            onChange={(event) => {
-                              setTitleEntry(event.target.value);
-                            }}
-                          />
-                          <h5></h5>
-                          <input
-                            type="text"
-                            className="input-one-c"
-                            placeholder="Language Used"
-                            // required
-                            value={languageEntry}
-                            onChange={(event) => {
-                              setLanguageEntry(event.target.value);
-                            }}
-                          />
-                          <Editor
-                            apiKey="ba2mzqsjqzq6lv0fu4numgypg3j9125otxy4rpzttx7vji3q"
-                            initialValue={initialVal}
-                            className="custom-editor"
-                            onEditorChange={handleEditorChange}
-                            value={entry}
-                            onChange={(event) => {
-                              console.log(event.target.value);
-                              setEntry(event.target.value);
-                            }}
-                            init={{
-                              height: 300,
-                              max_height: 340,
-                              menubar: true,
-                              plugins: [
-                                'advlist autolink lists link image',
-                                'charmap print preview anchor help',
-                                'searchreplace visualblocks code',
-                                'insertdatetime media table paste wordcount',
-                              ],
-                              toolbar:
-                                'undo redo | formatselect | bold italic | \
-                                alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | link image',
-                              content_style:
-                                'body { font-family: Arial, sans-serif; font-size: 14px }',
-                            }}
-                          />
-                          <input
-                            type="file"
-                            className="input-one-image"
-                            accept="image/*"
-                            value={image}
-                            onChange={(event) => {
-                              setImage(event.target.value);
-                            }}
-                          />
-                        </div>
-                        <div className="btn">
-                          <button
-                            type="submit"
-                            className="login-button"
-                            onClick={addComment}
-                          >
-                            Submit
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            )}
           </div>
       </div>
-
+      {showOverlay}
       <div className="input-container">
         <input type="text" className="input-bar" placeholder="Search APIs..." />
       </div>
