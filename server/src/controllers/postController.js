da3d7772c1f8fada19ac4f01fe6b4e8719cdd23d
Tristan Keester <tristankeester@gmail.com>
2023-07-19 19:52:14 -0500

    Minor alteration to SQL query in findPostsByTech

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -115,13 +115,9 @@ postController.findPostsByUser = async (req, res, next) => {
 postController.findPostsByTech = async (req, res, next) => {
   // Get all post with req.params.id == techId
   // Attach to res.locals.postList;
-  const postsByTech = await db.query(`SELECT techs.tech_id, posts.tech
-                                      FROM techs 
-                                      LEFT JOIN posts 
-                                      ON techs.tech_id = posts.tech `)
   console.log(postsByTech);
   const techId = req.params.id;
-  const lookupText = 'SELECT * FROM posts WHERE tech = $1';
+  const lookupText = 'SELECT posts.*, users.name FROM posts LEFT JOIN users ON posts.uploader = users.user_id WHERE tech = $1';
   const lookupVals = [techId];
   try {
     const { rows } = await db.query(lookupText, lookupVals);
