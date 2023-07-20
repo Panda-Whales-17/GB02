49f752f984a6f7e11b42f83fa4ef5781f55767dd
Tristan Keester <tristankeester@gmail.com>
2023-07-20 15:47:04 -0500

    Added test for the tech tiles

--- tests/HomePage.test.jsx
+++ tests/HomePage.test.jsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
 
@@ -21,6 +21,7 @@ describe('Testing the page layouts', () => {
     );
   })
 
+  // You have to comment out rehypeRaw from helper-functions.js and remove it from the react-markdown plugins key to get most of these tests to work
   test('Renders the home page', () => {
     expect(app.getByText('Cohort: CTRI 17')).toBeTruthy();
     expect(app.getByText('+ ADD TECH')).toBeTruthy();
@@ -38,4 +39,12 @@ describe('Testing the page layouts', () => {
     expect(screen.getByPlaceholderText('Add Image URL')).toBeTruthy();
     expect(screen.getByText('Submit!')).toBeTruthy();
   })
+
+  test('Renders the tiles containing each tech info', async () => {
+
+    // This is set up for mock data in Home.jsx
+    expect(screen.getByText('Google Maps API')).toBeTruthy();
+    expect(screen.getByText('Google Maps API allows you to embed maps into your website or application and customize them to fit your needs.')).toBeTruthy();
+    expect(screen.getByText('Posts')).toBeTruthy();
+  });
 })
