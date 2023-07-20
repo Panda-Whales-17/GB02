579b7aec2cc4518332e42da8ea0c5150014f1e3f
Tristan Keester <tristankeester@gmail.com>
2023-07-20 11:29:31 -0500

    Added userEvent module

--- tests/HomePage.test.jsx
+++ tests/HomePage.test.jsx
@@ -4,9 +4,10 @@
 
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
+
 import App from '../client/App.jsx';
 
 describe('Testing the page layouts', () => {
@@ -26,7 +27,10 @@ describe('Testing the page layouts', () => {
     expect(app.getByPlaceholderText("Search APIs...")).toBeTruthy();
   });
 
-  test('Can add a new tech', () => {
-    
+  test('Renders make tech form when clicking the button', () => {
+    const button = app.getByText('+ ADD TECH');
+
+    userEvent.click(button);
+    expect(app.getByText(''))
   })
 })
